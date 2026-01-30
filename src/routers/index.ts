e3ff3f91ffc1fd77a29867df12d6c55731e27cbd
Kateryna <kunieva.kateryna@gmail.com>
2026-01-30 17:36:35 +0100

    Added health check endpoint

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,6 +3,10 @@ import episode from '../episode/episode.router';
 
 const router = express.Router();
 
+router.get('/api/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
 router.use('/api/', episode);
 
 export default router;
